63b28d6cde327cce2220097453f918b1871c946e
Benjamin Baraza <barazabenjamin58@gmail.com>
2026-04-24 14:59:25 +0300

    main

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <AuthProvider>
       <App />
-    </StrictMode>,
-  );
-}
+    </AuthProvider>
+  </BrowserRouter>,
+);
